75555cd81d019dc07e67ba325943fe12c7aa944c
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-08-24 12:02:24 -0700

    Greatly improving readability for sum of multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,10 +1,3 @@
 pub fn sum_of_multiples(limit: usize, numbers: &Vec<usize>) -> usize {
-    (1..limit).fold(0, |sum, x| {
-        sum +
-            if numbers.iter().any(|n| x % n == 0) {
-                x
-            } else {
-                0
-            }
-    })
+    (1..limit).filter(|x| numbers.iter().any(|n| x % n == 0)).sum()
 }
