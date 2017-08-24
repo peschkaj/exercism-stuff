1ecfdb37523a7babfb5d73aade78d3fc4d62fe4c
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-08-24 11:08:21 -0700

    Revision of sum of squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -3,9 +3,9 @@ pub fn difference(n: usize) -> usize {
 }
 
 pub fn square_of_sum(n: usize) -> usize {
-    (1..n+1).sum::<usize>().pow(2)
+    ((n + 1) * n / 2).pow(2)
 }
 
 pub fn sum_of_squares(n: usize) -> usize {
-    (1..n+1).map(|x| x.pow(2)).sum::<usize>()
+    (1..n+1).fold(0, |acc, x| acc + x.pow(2))
 }
