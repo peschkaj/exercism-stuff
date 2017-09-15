292da7d4add63eeb7b537467dd8c2dbb3ba34042
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-09-15 15:06:18 -0700

    Initial implementation of scrabble scores in Rust

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn score(word: &str) -> usize {
+    let mut score: usize = 0;
+
+    let word = word.to_lowercase();
+
+    for c in word.chars() {
+        score += match c {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+            'd' | 'g' => 2,
+            'b' | 'c' | 'm' | 'p' => 3,
+            'f' | 'h' | 'v' | 'w' | 'y' => 4,
+            'k' => 5,
+            'j' | 'x' => 8,
+            'q' | 'z' => 10,
+            _ => 0,
+        }
+    }
+
+    score
+}
