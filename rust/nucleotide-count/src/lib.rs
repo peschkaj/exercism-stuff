b552fa8f1edb045701dc507a4acd6eef8f6188c8
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-09-15 16:56:59 -0700

    First attempt at nucleotide count

---
+++ rust/nucleotide-count/src/lib.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+pub fn count(nucleotide: char, dna: &'static str) -> Result<usize, &'static str> {
+    if nucleotide != 'A' && nucleotide != 'C' && nucleotide != 'G' && nucleotide != 'T' ||
+        dna.chars().any(
+            |x| x != 'A' && x != 'C' && x != 'G' && x != 'T',
+        )
+    {
+        return Err("mismatch");
+    }
+
+    Ok(dna.chars().filter(|x| x == &nucleotide).collect::<String>().len())
+}
+
+pub fn nucleotide_counts<'a>(dna: &'a str) -> Result<HashMap<char, usize>, &'static str> {
+    if dna.chars().any(|x|
+                       x != 'A' && x != 'C' && x != 'G' && x != 'T') {
+        return Err("nope!");
+    }
+
+    let mut seen = HashMap::new();
+    seen.entry('A').or_insert(0 as usize);
+    seen.entry('C').or_insert(0 as usize);
+    seen.entry('G').or_insert(0 as usize);
+    seen.entry('T').or_insert(0 as usize);
+
+    for nucleotide in dna.chars() {
+        let e = seen.entry(nucleotide).or_insert(0 as usize);
+        *e += 1;
+    }
+    Ok(seen)
+}
