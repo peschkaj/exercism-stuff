523c7bc6b38947f160649d4cc0f03a4a880ee63b
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-09-19 11:07:15 -0700

    LSP implementation

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -1,12 +1,22 @@
 #[derive(Debug)]
 pub enum LSPError {
     IllegalLength,
+    IllegalCharacters,
 }
 
 pub fn lsp(number: &str, length: usize) -> Result<u32, LSPError> {
     if number.len() < length {
         return Err(LSPError::IllegalLength);
     }
+
+    if length == 0 || number.is_empty() {
+        return Ok(1);
+    }
+
+    if number.chars().any(|c| !c.is_numeric()) {
+        return Err(LSPError::IllegalCharacters);
+    }
+
     let digits: Vec<u32> = number.chars().filter_map(|c| c.to_digit(10)).collect();
 
     Ok(
--- rust/largest-series-product/tests/largest-series-product.rs
+++ rust/largest-series-product/tests/largest-series-product.rs
@@ -13,56 +13,47 @@ fn find_the_largest_product_when_span_equals_length() {
 }
 
 #[test]
-
 fn find_the_largest_product_of_two_with_numbers_in_order() {
     assert_eq!(72, lsp("0123456789", 2).unwrap());
 }
 
 #[test]
-#[ignore]
 fn find_the_largest_product_of_two_with_numbers_not_in_order() {
     assert_eq!(48, lsp("576802143", 2).unwrap());
 }
 
 #[test]
-#[ignore]
 fn find_the_largest_product_of_three_with_numbers_in_order() {
     assert_eq!(504, lsp("0123456789", 3).unwrap());
 }
 
 #[test]
-#[ignore]
 fn find_the_largest_product_of_three_with_numbers_not_in_order() {
     assert_eq!(270, lsp("1027839564", 3).unwrap());
 }
 
 #[test]
-#[ignore]
 fn find_the_largest_product_of_five_with_numbers_in_order() {
     assert_eq!(15120, lsp("0123456789", 5).unwrap());
 }
 
 #[test]
-#[ignore]
 fn span_of_six_in_a_large_number() {
     assert_eq!(23520,
                lsp("73167176531330624919225119674426574742355349194934", 6).unwrap());
 }
 
 #[test]
-#[ignore]
 fn returns_zero_if_number_is_zeros() {
     assert_eq!(0, lsp("0000", 2).unwrap());
 }
 
 #[test]
-#[ignore]
 fn returns_zero_if_all_products_are_zero() {
     assert_eq!(0, lsp("99099", 3).unwrap());
 }
 
 #[test]
-#[ignore]
 fn a_span_is_longer_than_number_is_an_error() {
     assert!(lsp("123", 4).is_err());
 }
@@ -80,25 +71,21 @@ fn a_span_is_longer_than_number_is_an_error() {
 // So LSP('123', 4) really DOES take the max of an empty list.
 // So LSP('123', 4) errors and LSP('', 0) does NOT.
 #[test]
-#[ignore]
 fn an_empty_string_and_no_span_returns_one() {
     assert_eq!(1, lsp("", 0).unwrap());
 }
 
 #[test]
-#[ignore]
 fn a_non_empty_string_and_no_span_returns_one() {
     assert_eq!(1, lsp("123", 0).unwrap());
 }
 
 #[test]
-#[ignore]
 fn empty_string_and_non_zero_span_is_an_error() {
     assert!(lsp("", 1).is_err());
 }
 
 #[test]
-#[ignore]
 fn a_string_with_non_digits_is_an_error() {
     assert!(lsp("1234a5", 2).is_err());
 }
