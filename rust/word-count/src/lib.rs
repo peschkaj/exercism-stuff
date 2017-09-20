0689edb5d63b6815c2c2292a59e83f038c0a181b
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-09-20 06:45:00 -0700

    word count is now a one-liner

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,18 +1,11 @@
 use std::collections::HashMap;
 
 pub fn word_count(sentence: &str) -> HashMap<String, u32> {
-    let mut word_counts = HashMap::new();
-
-    let words: Vec<String> = sentence
+    sentence
         .split(|c: char| !c.is_alphanumeric())
-        .filter(|&w| w != "")
-        .map(|w| w.to_lowercase())
-        .collect();
-
-    for word in words.iter() {
-        let word = word.to_lowercase();
-        *word_counts.entry(word).or_insert(0) += 1;
-    }
-
-    word_counts
+        .filter(|s| !s.is_empty())
+        .fold(HashMap::new(),|mut map, s| {
+            *map.entry(s.to_lowercase()).or_insert(0) += 1;
+            map
+        })
 }
