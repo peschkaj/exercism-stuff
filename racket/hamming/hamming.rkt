4ee3dcd8b7221badfe5896cf7c42a54b8f1eab27
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-10-09 14:35:21 -0700

    hamming clean up

--- racket/hamming/hamming.rkt
+++ racket/hamming/hamming.rkt
@@ -5,7 +5,6 @@
 (define (hamming-distance upper lower)
   (if (not (equal? (string-length upper)
                    (string-length lower))) (exn:fail)
-
       (for/sum ([a upper]
                 [b lower]
                 #:when (not (equal? a b)))
