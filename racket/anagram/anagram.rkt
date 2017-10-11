8aaaa879c247810539d31465e2f27eda2cfebe79
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-10-10 17:32:46 -0700

    removing old code

--- racket/anagram/anagram.rkt
+++ racket/anagram/anagram.rkt
@@ -16,17 +16,3 @@
        (not (string=? word candidate))
        (string-ci=? (sort-string word)
                     (sort-string candidate))))
-
-;; (define (anagrams-for word candidates)
-;;   (if (contains-word? candidates word) '()
-;;       (let* ([perms (remove word (permutations-of (string-downcase word)))])
-;;         ;; check if perms contains any words from candidate
-;;         (for/list ([c candidates]
-;;                    #:when (contains-word? perms c))
-;;           c))))
-
-;; (define (permutations-of s)
-;;   (map list->string (permutations (string->list s))))
-
-;; (define (contains-word? lst word)
-;;   (number? (index-of lst (string-downcase word))))
