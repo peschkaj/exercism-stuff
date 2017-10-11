f891d7af49d6fc8420da244a4db3d06e69753827
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-10-11 14:46:18 -0700

    moving ETL to an explicit contract

--- racket/etl/etl.rkt
+++ racket/etl/etl.rkt
@@ -2,13 +2,8 @@
 
 (provide etl)
 
-(define (etl score-hash)
-  (let* ([key-list (for/list ([key (hash-keys score-hash)]
-                              #:when (<= key 0))
-                     key)]
-         [key-count (length key-list)])
-
-    (if (> key-count 0) (exn:fail:contract)
-        (for*/hash ([(score letters) score-hash]
-                   [letter letters])
-          (values (string-downcase letter) score)))))
+(define/contract (etl mapping)
+  (-> (hash/c positive? (listof string?)) hash?)
+  (for*/hash ([(key vs) (in-hash mapping)]
+              [value vs])
+    (values (string-downcase value) key)))
