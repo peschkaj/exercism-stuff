ee818ee6dcdb51dd0d56232cd5ed16f62eac5e1e
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-06-06 13:21:48 -0700

    Updates RNA to use mapM

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,12 +1,10 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Maybe String
-toRNA xs | null xs                                 = Nothing
-         | not $ all (`elem` ['A', 'C', 'T', 'G']) xs = Nothing
-         | otherwise                               = Just $ toRNA' xs
-  where toRNA' [] = []
-        toRNA' (d:ds) = case d of 'G' -> 'C' : toRNA' ds
-                                  'C' -> 'G' : toRNA' ds
-                                  'T' -> 'A' : toRNA' ds
-                                  'A' -> 'U' : toRNA' ds
-                                  _   -> error "That's not DNA"
+toRNA = mapM toRNA'
+  where toRNA' :: Char -> Maybe Char
+        toRNA' 'G' = Just 'C'
+        toRNA' 'C' = Just 'G'
+        toRNA' 'T' = Just 'A'
+        toRNA' 'A' = Just 'U'
+        toRNA'  _  = Nothing
