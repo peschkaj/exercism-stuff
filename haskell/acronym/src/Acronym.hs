3910d1bf91663450e975d83544c55c850f24022b
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-06-15 10:43:24 -0700

    serious clean up for acronym

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -7,5 +7,7 @@ import Data.List.Split
 
 abbreviate :: String -> String
 abbreviate = concatMap (fmap toUpper . collect) . splitOneOf " -"
-   where collect wd = if null uppers then pure (head wd) else uppers
-           where uppers = if all isUpper wd then pure (head wd) else filter isUpper wd
+   where collect wd | null uppers = pure $ head wd
+                    | otherwise   = uppers
+           where uppers | all isUpper wd = pure (head wd)
+                        | otherwise      = filter isUpper wd
