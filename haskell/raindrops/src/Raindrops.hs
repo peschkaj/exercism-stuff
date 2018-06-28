aa8bf8356df8ce0a37886daa835707e691d2bdda
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-06-28 14:43:36 -0700

    Cleaning up Raindrops
    - Uses data instead of brute force calculation to generate the sounds

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -1,13 +1,8 @@
 module Raindrops (convert) where
 
-
 convert :: Int -> String
-convert n | drops factors == "" = show n
-          | otherwise           = drops factors
-  where factors = filter (`isFactorOf` n) [1 .. n]
-        isFactorOf x y = y `mod` x == 0
-        drops (x:xs) | x == 3    = "Pling" ++ drops xs
-                     | x == 5    = "Plang" ++ drops xs
-                     | x == 7    = "Plong" ++ drops xs
-                     | otherwise = drops xs
-        drops []                 = ""
+convert n | str == "" = show n
+          | otherwise = str
+  where str = concat $ zipWith f [3,5,7] ["Pling", "Plang", "Plong"]
+        f digit message | mod n digit == 0 = message
+                        | otherwise        = ""
