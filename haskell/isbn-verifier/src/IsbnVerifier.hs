57d1e09290533e250c3bf6c5e9254fd7d8fff0e3
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-07-10 18:46:59 -0700

    Cleaning up haskell isbn verifier

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -5,24 +5,27 @@ import Data.Char (isDigit, digitToInt)
 import Data.List (elemIndex)
 
 isbn :: String -> Bool
-isbn xs | not validXPosition              = False
-        | length (formattedIsbn xs) == 10 = valid . checksum $ formattedIsbn xs
-        | otherwise                       = False
+isbn xs
+  | not validXPosition  = False
+  | length isbn == 10   = valid $ checksum isbn
+  | otherwise           = False
   where
-    validXPosition = case elemIndex 'X' (formattedIsbn xs) of Nothing -> True
-                                                              Just x  -> x == 9
+    isbn = formattedIsbn xs
+    validXPosition = case elemIndex 'X' isbn of Nothing -> True
+                                                Just x  -> x == 9
 
     formattedIsbn :: String -> String
-    formattedIsbn = filter (\x -> isDigit x || x == 'X') . filter(/= '_')
+    formattedIsbn = filter (\x -> isDigit x || x == 'X')
 
     checksum :: String -> Int
     checksum ds = sum $ mapWithIndex (\idx x -> (10 - idx) * x) $ fromList $ toDigits ds
 
     toDigits :: String -> [Int]
     toDigits [] = []
-    toDigits (d:ds) | isDigit d = digitToInt d : toDigits ds
-                    | d == 'X'  = 10 : toDigits ds
-                    | otherwise = 0 : toDigits ds
+    toDigits (d:ds)
+      | isDigit d = digitToInt d : toDigits ds
+      | d == 'X'  = 10 : toDigits ds
+      | otherwise = 0 : toDigits ds
 
     valid :: Int -> Bool
     valid n = n `mod` 11 == 0
