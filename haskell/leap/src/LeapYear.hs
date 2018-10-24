63dc1abb9fa742f75f6e4cc93f606d15cd430410
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-10-23 18:13:28 -0700

    Moving to guards

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,7 +1,12 @@
-module LeapYear (isLeapYear) where
+module LeapYear
+        ( isLeapYear
+        )
+where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = year `mod` 4 == 0 &&
-  year `mod` 100 /= 0 ||
-  year `mod` 400 == 0
+isLeapYear year | isDivisibleBy 400 = True
+                | isDivisibleBy 100 = False
+                | isDivisibleBy 4   = True
+                | otherwise         = False
+        where isDivisibleBy x = year `mod` x == 0
 
