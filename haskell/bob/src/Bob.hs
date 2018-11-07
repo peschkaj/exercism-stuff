a936223df5f33f97bd902155e4dabc1539515e54
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-11-07 07:43:05 -0800

    Bob clean up

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -4,6 +4,7 @@ module Bob
 where
 
 import           Data.Char
+import           Data.List                      ( isSuffixOf )
 
 data Query = Question
            | Shout
@@ -19,20 +20,19 @@ responseFor xs = case questionType xs of
         ShoutQuestion  -> "Calm down, I know what I'm doing!"
         NoQuery        -> "Fine. Be that way!"
         EverythingElse -> "Whatever."
+
+questionType :: String -> Query
+questionType xs | null word             = NoQuery
+                | ("?" `isSuffixOf` word) && not (isShout word) = Question
+                | "?" `isSuffixOf` word = ShoutQuestion
+                | isShout word          = Shout
+                | otherwise             = EverythingElse
     where
+        word = trim xs
+        trim = unwords . words
 
-        questionType :: String -> Query
-        questionType xs | null word        = NoQuery
-                        | (last word == '?') && not (isShout word) = Question
-                        | last word == '?' = ShoutQuestion
-                        | isShout word     = Shout
-                        | otherwise        = EverythingElse
-            where
-                word = trim xs
-                trim = unwords . words
-
-        isShout :: String -> Bool
-        isShout s | word == ""     = False
-                  | all isUpper word = True
-                  | otherwise      = False
-                where word = filter isAlpha s
+isShout :: String -> Bool
+isShout s | word == ""       = False
+          | all isUpper word = True
+          | otherwise        = False
+        where word = filter isAlpha s
