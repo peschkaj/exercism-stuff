89b1f2b98b520ccaaef804d2c533e1059b524946
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-11-12 12:59:25 -0800

    Further improvements to pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,12 +1,9 @@
 module Pangram (isPangram) where
 
-import Data.Char
-import Data.List
+import Data.Char (toLower)
+import Data.List ((\\))
 
 isPangram :: String -> Bool
-isPangram text = letters == ['a'..'z']
-  where letters = sort . uniq $ filter isAsciiLower $ map toLower text
+isPangram = null . (['a' .. 'z'] \\) . map toLower
 
-uniq :: Eq a => [a] -> [a]
-uniq = foldl (\acc item -> if item `elem` acc then acc else acc ++ [item]) []
 
