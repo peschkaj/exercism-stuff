6e4c54ee12600d1e4317c13339bdd2645c1b1e94
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-01-02 15:10:03 -0800

    Reformatting with a different reformattering

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -3,23 +3,21 @@ module Phone
         )
 where
 
-import           Data.Char                      ( isDigit )
-import           Data.Sequence                  ( mapWithIndex
-                                                , fromList
-                                                )
+import           Data.Char     (isDigit)
+import           Data.Sequence (fromList, mapWithIndex)
 
 number :: String -> Maybe String
 number [] = Nothing
 number xs = pn $ filter isDigit xs
-    where
-        pn [] = Nothing
-        pn ds@(y : ys) | y == '1'  = pn ys
-                       | valid ds  = Just ds
-                       | otherwise = Nothing
+  where
+    pn [] = Nothing
+    pn ds@(y : ys) | y == '1'  = pn ys
+                   | valid ds  = Just ds
+                   | otherwise = Nothing
 
-        valid ds | length ds == 10 = all (== True) $ mapWithIndex checkDigit $ fromList ds
-                 | otherwise       = False
+    valid ds | length ds == 10 = all (== True) $ mapWithIndex checkDigit $ fromList ds
+             | otherwise       = False
 
-        checkDigit pos d | pos == 0 && d < '2' = False
-                         | pos == 3 && d < '2' = False
-                         | otherwise           = True
+    checkDigit pos d | pos == 0 && d < '2' = False
+                     | pos == 3 && d < '2' = False
+                     | otherwise           = True
